4b355d9bcb309c269b8369bb4be02a511e6e212f
Mgccl <mgcclx@gmail.com>
2010-05-19 23:41:22 -0400

    More OJs

--- ac_count.php
+++ ac_count.php
@@ -1,8 +1,9 @@
 <?php
 require('config.php');
+$sum = 0;
 $oj = array_keys($id);
 for($i = 0;$i<count($oj);$i++){
 	require('oj/'.$oj[$i].'.php');
 	$ac[$oj[$i]] = call_user_func($oj[$i].'_ac_count',$id[$oj[$i]]);
+	$sum += $ac[$oj[$i]];
 }
-print_r($ac);
--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
 //id for every oj
 $id['pe']='mgccl';
+$id['timus']='79684'; //URAL
 $id['uva']='28091';
+$id['sgu'] = '040608';
+$id['poj'] = 'Mgccl';
+$id['zoj'] = '68480';
